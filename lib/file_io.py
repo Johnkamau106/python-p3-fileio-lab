cdbb87042d3c0c4d4da633576dd7b604023ece04
Your Name <johnkamau106@gmail.com>
2025-05-31 23:06:51 +0300

    Implement file operations in file_io.py: write, append, and read functions

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    # Add .txt extension to file_name
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    # Add .txt extension to file_name
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    # Add .txt extension to file_name
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
